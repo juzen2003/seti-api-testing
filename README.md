b916964e58f6c13f876412e1cf1fdd3fcdc15e29
Yu-Jen Chang <juzen2003@gmail.com>
2018-11-09 18:19:26 -0800

    Add more apis

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-### API testing notes
-## return file format testing
+## API testing notes
+### return file format testing
 * Make sure all supported return formats are valid
 
-## Setup
+### Setup
 * virtualenv venv
 * source venv/bin/activate
 * pip3 install requests
 
-## Reference
-* response object: http://docs.python-requests.org/en/v1.0.0/api/
+### Reference
 * usage of request: http://docs.python-requests.org/en/master/user/quickstart/
+* response object: http://docs.python-requests.org/en/v1.0.0/api/
--- test_api.py
+++ test_api.py
@@ -2,26 +2,43 @@ import json
 import requests
 
 file_format = ["json", "html","zip", "csv"]
-# api_url_base = "https://tools.pds-rings.seti.org/opus/api/meta/result_count.%s" %(file_format)
-api_url_base = "https://tools.pds-rings.seti.org/opus/api/meta/result_count."
-payload = {'planet': 'Saturn', 'target': 'Pluto'} # slugs
-# print(api_url_base)
+# slugs
+payload1 = {"planet": "Saturn", "target": "Pluto"}
+payload2 = {"planet": "Earth", "target": "Pluto", "mission": "Hubble"}
+payload3 = {"planet": "Earth", "target": "Moon"}
 
-for format in file_format:
-    api = api_url_base + format
-    res = requests.get(api, params=payload);
-    http_status_code = res.status_code
-    # print(http_status_code)
-    print(res.url)
+# Getting Information about Data
+# api/meta/result_count.[fmt]
+api_count_base = "https://tools.pds-rings.seti.org/opus/api/meta/result_count."
+# api/meta/mults/[param].[fmt]
+api_mults_base = "https://tools.pds-rings.seti.org/opus/api/meta/mults/mission." #for now we use mission
+# api/meta/range/endpoints/[param].[fmt]
+api_endpoints_base = "https://tools.pds-rings.seti.org/opus/api/meta/range/endpoints/wavelength1." #for now we use wavelength1
 
-    if http_status_code == 200:
-        print("Response ok, http status code: %s" %(http_status_code))
-    elif http_status_code >= 400 and http_status_code < 500:
-        print("Bad request, http status code: %s" %(http_status_code))
-    elif http_status_code >= 500:
-        print("Server error, http status code: %s" %(http_status_code))
-    else:
-        print("Error, http status code: %s" %(http_status_code))
+
+def test_api_return_format(api_url_base, payload):
+    print("Testing API: %s" %(api_url_base) + "[fmt]")
+    for format in file_format:
+        api = api_url_base + format
+        res = requests.get(api, params=payload);
+        http_status_code = res.status_code
+
+        if http_status_code == 200:
+            print("%s: Response ok, http status code: %s" %(format, http_status_code))
+        elif http_status_code >= 400 and http_status_code < 500:
+            print("%s: Bad request, http status code: %s" %(format, http_status_code))
+        elif http_status_code >= 500:
+            print("%s: Server error, http status code: %s" %(format, http_status_code))
+        else:
+            print("%s: Error, http status code: %s" %(format, http_status_code))
+
+
+
+# main function
+if __name__ == '__main__':
+    test_api_return_format(api_count_base, payload1)
+    test_api_return_format(api_mults_base, payload2)
+    test_api_return_format(api_endpoints_base, payload3)
 
     # try:
     #     res.raise_for_status()
